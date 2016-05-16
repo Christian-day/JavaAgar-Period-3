ce5343d43528f92db291060d94b70a4762d0fb96
C. Day, Deerfield Academy <cday@deerfield.edu>
2016-05-16 12:24:32 -0400

    Mr. Day comments

--- src/da/agar/Growable.java
+++ src/da/agar/Growable.java
@@ -2,8 +2,18 @@ package da.agar;
 
 public interface Growable {
 	
+	/**
+	 * Display the current size of the object.  It is unclear what is meant by "size", so it is possible that 
+	 * getSize() may return something other than an int
+	 * @return The size of the object
+	 */
 	public int getSize();
 	
+	/**
+	 * Set the size of the object.  It is unclear what is meant by "size", so it is possible that
+	 * setSize() may take some object other than int as its parameter.
+	 * @param size
+	 */
 	public void setSize( int size );
 
 }
