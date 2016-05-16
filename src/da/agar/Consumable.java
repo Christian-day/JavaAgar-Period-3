14b87ee476d7f0452778ac845347ed1059559e7c
C. Day, Deerfield Academy <cday@deerfield.edu>
2016-05-16 10:53:19 -0400

    Mr. Day's commentary on the Consumable class

--- src/da/agar/Consumable.java
+++ src/da/agar/Consumable.java
@@ -1,8 +1,17 @@
 package da.agar;
 
 public interface Consumable {
+	/**
+	 * 
+	 * @return the number of points this Consumable is worth.  Does NOT consume the object
+	 */
 	public int getPoints();
 	
+	/**
+	 * consume the object.  When a food object is consumed it should no longer display on the Game Board.  The number
+	 * of points is awarded to the object that ate the food.
+	 * @return the number of points the consumed object is worth
+	 */
 	public int Consume();
 	
 }
